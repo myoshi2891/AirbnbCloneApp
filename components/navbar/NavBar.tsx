7cb557f307281df9d9d35f86eb261d2ab2203b54
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-12-30 18:21:07 +0900

    Navbar Structure

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,5 +1,21 @@
+import NavSearch from "./NavSearch";
+import LinksDropdown from "./LinksDropdown";
+import DarkMode from "./DarkMode";
+import Logo from "./Logo";
+
 function NavBar() {
-	return <div>NavBar</div>;
+	return (
+		<nav className="border-b">
+			<div className="container flex flex-col sm:flex-row justify-between sm:items-center flex-wrap gap-4 py-8">
+				<Logo />
+				<NavSearch />
+				<div className="flex gap-4 items-center">
+					<DarkMode />
+					<LinksDropdown />
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default NavBar;
