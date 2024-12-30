59edf3829db1f166bd5ddbad92bb6972570bbb64
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-12-30 18:29:06 +0900

    Logo and NavSearch Components

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+import { LuTent } from "react-icons/lu";
+import { Button } from "../ui/button";
 function Logo() {
-	return <div>Logo</div>;
+	return (
+		<Button size="icon" asChild>
+			<Link href="/">
+				<LuTent className="h-6 w-6" />
+			</Link>
+		</Button>
+	);
 }
 
 export default Logo;
