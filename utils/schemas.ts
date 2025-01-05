621433bea744ea04e863ca8d453a08af9c95b5e4
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-05 14:09:03 +0900

    Zod SafeParse Method

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -3,7 +3,13 @@ import { ZodSchema } from "zod";
 
 export const profileSchema = z.object({
 	// firstName: z.string().max(5, { message: "Max length is 5 characters" }),
-	firstName: z.string().min(2),
-	lastName: z.string().min(2),
-	username: z.string().min(2),
+	firstName: z
+		.string()
+		.min(2, { message: "first name must be at least 2 characters" }),
+	lastName: z
+		.string()
+		.min(2, { message: "last name must be at least 2 characters" }),
+	username: z
+		.string()
+		.min(2, { message: "user name must be at least 2 characters" }),
 });
