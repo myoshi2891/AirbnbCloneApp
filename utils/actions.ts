5b04c1ea692f97effc836fb8896c690e7749f0d3
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-05 13:38:27 +0900

    Fetch User Profile

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,15 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+const getAuthUser = async () => {
+	const user = await currentUser();
+	if (!user) {
+		throw new Error("You must be logged in to access this route...");
+	}
+	if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+	return user;
+};
+
 export const createProfileAction = async (
 	prevState: any,
 	formData: FormData
@@ -54,4 +63,22 @@ export const fetchProfileImage = async () => {
 	});
 
 	return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+	const user = await getAuthUser();
+	const profile = await db.profile.findUnique({
+		where: {
+			clerkId: user.id,
+		},
+	});
+	if (!profile) redirect("/profile/create");
+	return profile;
+};
+
+export const updateProfileAction = async (
+	prevState: any,
+	formData: FormData
+): Promise<{ message: string }> => {
+	return { message: "update profile action" };
 };
