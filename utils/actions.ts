bd05b85bbf05fe10973449290599c37f136b2573
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-05 14:33:59 +0900

    ValidateWithZodSchema - Helper Function

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { profileSchema } from "./schemas";
+import { profileSchema, validateWithZodSchema } from "./schemas";
 import db from "./db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
@@ -31,13 +31,13 @@ export const createProfileAction = async (
 		if (!user) throw new Error("Please login to create a profile");
 
 		const rawData = Object.fromEntries(formData);
-		const validatedField = profileSchema.parse(rawData);
+		const validatedFields = validateWithZodSchema(profileSchema, rawData);
 		await db.profile.create({
 			data: {
 				clerkId: user.id,
 				email: user.emailAddresses[0].emailAddress,
 				profileImage: user.imageUrl ?? "",
-				...validatedField,
+				...validatedFields,
 			},
 		});
 		await clerkClient.users.updateUserMetadata(user.id, {
@@ -84,25 +84,17 @@ export const updateProfileAction = async (
 	prevState: any,
 	formData: FormData
 ): Promise<{ message: string }> => {
-
 	const user = await getAuthUser();
 
 	try {
 		const rawData = Object.fromEntries(formData);
-		const validatedFields = profileSchema.safeParse(rawData);
-
-		if (!validatedFields.success) {
-			const errors = validatedFields.error.errors.map(
-				(error) => error.message
-			);
-			throw new Error(errors.join(","));
-		}
+		const validatedFields = validateWithZodSchema(profileSchema, rawData);
 
 		await db.profile.update({
 			where: {
 				clerkId: user.id,
 			},
-			data: validatedFields.data,
+			data: validatedFields,
 		});
 		revalidatePath("/profile");
 		return { message: "Profile updated successfully!" };
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -13,3 +13,18 @@ export const profileSchema = z.object({
 		.string()
 		.min(2, { message: "user name must be at least 2 characters" }),
 });
+
+
+export function validateWithZodSchema<T>(
+	schema: ZodSchema<T>,
+	data: unknown
+): T {
+	const result = schema.safeParse(data);
+
+	if (!result.success) {
+		const errors = result.error.errors.map((error) => error.message);
+		throw new Error(errors.join(","));
+	}
+
+	return result.data;
+}
