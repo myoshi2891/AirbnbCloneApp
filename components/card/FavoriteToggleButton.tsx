3f511f1963a64a642c2c3be6f2e881acdd5028cd
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-12 14:09:32 +0900

    Favorites SignIn Button

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,7 +1,11 @@
 import { FaHeart } from "react-icons/fa";
 import { Button } from "../ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { CardSignInButton } from "../form/Buttons";
 
 function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+	const { userId } = auth();
+	if (!userId) return <CardSignInButton />;
 	return (
 		<Button size="icon" variant="outline" className="p-2 cursor-pointer">
 			<FaHeart />
@@ -10,3 +14,4 @@ function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
 }
 
 export default FavoriteToggleButton;
+("");;
