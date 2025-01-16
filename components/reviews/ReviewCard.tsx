2e9c89437a157cf274082b3d31cc222803d9068b
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-16 21:35:01 +0900

    ReviewCard Component

--- components/reviews/ReviewCard.tsx
+++ components/reviews/ReviewCard.tsx
@@ -1,5 +1,45 @@
-function ReviewCard({ reviewInfo }: { reviewInfo: any }) {
-	return <div>ReviewCard</div>;
+import { Card, CardContent, CardHeader } from "../ui/card";
+import Rating from "./Rating";
+import Comment from "./Comment";
+import Image from "next/image";
+
+type ReviewCardProps = {
+	reviewInfo: {
+		comment: string;
+		rating: number;
+		name: string;
+		image: string;
+	};
+	children?: React.ReactNode;
+};
+
+function ReviewCard({ reviewInfo, children }: ReviewCardProps) {
+	return (
+		<Card className="relative">
+			<CardHeader>
+				<div className="flex items-center">
+					<Image
+						src={reviewInfo.image}
+						alt="profile"
+						width={48}
+						height={48}
+						className="w-12 h-12 rounded-full object-cover"
+					/>
+					<div className="ml-4">
+						<h3 className="text-sm font-bold capitalize mb-1">
+							{reviewInfo.name}
+						</h3>
+						<Rating rating={reviewInfo.rating} />
+					</div>
+				</div>
+			</CardHeader>
+			<CardContent>
+				<Comment comment={reviewInfo.comment} />
+			</CardContent>
+			{/* delete button  */}
+			<div className="absolute top-3 right-3">{children}</div>
+		</Card>
+	);
 }
 
 export default ReviewCard;
