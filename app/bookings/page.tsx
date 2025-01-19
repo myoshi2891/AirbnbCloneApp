9485c4f7eb3340b9cba13167aae8cc6063c4b3bf
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-19 14:46:42 +0900

    Bookings Page

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,5 +1,88 @@
-function BookingPage() {
-	return <div>BookingPage</div>;
+import EmptyList from "@/components/home/EmptyList";
+import CountryFlagAndName from "@/components/card/CountryFlagAndName";
+import Link from "next/link";
+
+import { formatDate, formatCurrency } from "@/utils/format";
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+
+import FormContainer from "@/components/form/FormContainer";
+import { IconButton } from "@/components/form/Buttons";
+import { fetchBookings, deleteBookingAction } from "@/utils/actions";
+
+async function BookingPage() {
+	const bookings = await fetchBookings();
+	if (bookings.length === 0) return <EmptyList />;
+
+	return (
+		<div className="mt-16">
+			<h4 className="mb-4 capitalize">
+				total bookings : {bookings.length}
+			</h4>
+			<Table>
+				<TableCaption>A list of your recent bookings</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Property Name</TableHead>
+						<TableHead>Country</TableHead>
+						<TableHead>Nights</TableHead>
+						<TableHead>Total</TableHead>
+						<TableHead>Check In</TableHead>
+						<TableHead>Check Out</TableHead>
+						<TableHead>Actions</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{bookings.map((booking) => {
+						const {
+							id,
+							orderTotal,
+							totalNights,
+							checkIn,
+							checkOut,
+						} = booking;
+						const {
+							id: propertyId,
+							name,
+							country,
+						} = booking.property;
+
+						const startDate = formatDate(checkIn);
+						const endDate = formatDate(checkOut);
+
+						return (
+							<TableRow key={id}>
+								<TableCell>
+									<Link
+										href={`/properties/${propertyId}`}
+										className="underline text-muted-foreground tracking-wide"
+									>
+										{name}
+									</Link>
+								</TableCell>
+								<TableCell>
+									<CountryFlagAndName countryCode={country} />
+								</TableCell>
+								<TableCell>{totalNights}</TableCell>
+								<TableCell>
+									{formatCurrency(orderTotal)}
+								</TableCell>
+								<TableCell>{startDate}</TableCell>
+								<TableCell>{endDate}</TableCell>
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
+		</div>
+	);
 }
 
 export default BookingPage;
