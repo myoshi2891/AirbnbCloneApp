8e230305f6ae134c455ceb00688f982349b5af8c
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-19 15:50:23 +0900

    Rentals Page

--- app/rentals/page.tsx
+++ app/rentals/page.tsx
@@ -1,5 +1,89 @@
-function RentalsPage() {
-	return <div>RentalsPage</div>;
+import EmptyList from "@/components/home/EmptyList";
+import { fetchRentals, deleteRentalAction } from "@/utils/actions";
+import Link from "next/link";
+
+import { formatCurrency } from "@/utils/format";
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+
+import FormContainer from "@/components/form/FormContainer";
+import { IconButton } from "@/components/form/Buttons";
+import { toNamespacedPath } from "node:path/win32";
+
+async function RentalsPage() {
+	const rentals = await fetchRentals();
+	if (rentals.length === 0) {
+		return (
+			<EmptyList
+				heading="No rentals to display"
+				message="Don't hesitate to create a rental."
+			/>
+		);
+	}
+	return (
+		<div className="mt-16">
+			<h4 className="mb-4 capitalize">
+				active properties : {rentals.length}
+			</h4>
+			<Table>
+				<TableCaption>A list of all your properties</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Property Name</TableHead>
+						<TableHead>Nightly Rate</TableHead>
+						<TableHead>Nightly Booked</TableHead>
+						<TableHead>Total Income</TableHead>
+						<TableHead>Actions</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{rentals.map((rental) => {
+						const { id: propertyId, name, price } = rental;
+						const { totalNightsSum, orderTotalSum } = rental;
+						return (
+							<TableRow key={propertyId}>
+								<TableCell>
+									<Link
+										href={`/properties/${propertyId}`}
+										className="underline text-muted-foreground tracking-wide"
+									>
+										{name}
+									</Link>
+								</TableCell>
+								<TableCell>{formatCurrency(price)}</TableCell>
+								<TableCell>{totalNightsSum || 0}</TableCell>
+								<TableCell>
+									{formatCurrency(orderTotalSum)}
+								</TableCell>
+								<TableCell className="flex items-center gap-x-2">
+									<Link href={`/rentals/${propertyId}/edit`}>
+										<IconButton actionType="edit" />
+									</Link>
+									<DeleteRental propertyId={propertyId} />
+								</TableCell>
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
+		</div>
+	);
+}
+
+function DeleteRental({ propertyId }: { propertyId: string }) {
+	const deleteRental = deleteRentalAction.bind(null, { propertyId });
+	return (
+		<FormContainer action={deleteRental}>
+			<IconButton actionType="delete" />
+		</FormContainer>
+	);
 }
 
 export default RentalsPage;
