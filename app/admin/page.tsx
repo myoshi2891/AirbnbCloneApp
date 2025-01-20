f7a638c565c72b08a0338aa91d0f7542937afee8
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-20 22:13:40 +0900

    Admin Page - Loading Components

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,22 @@
-function page() {
-	return <div>page</div>;
+import ChartsContainer from "@/components/admin/ChartsContainer";
+import {
+	ChartsLoadingContainer,
+	StatsLoadingContainer,
+} from "@/components/admin/Loading";
+import StatsContainer from "@/components/admin/StatsContainer";
+import { Suspense } from "react";
+
+function AdminPage() {
+	return (
+		<>
+			<Suspense fallback={<StatsLoadingContainer />}>
+				<StatsContainer />
+			</Suspense>
+			<Suspense fallback={<ChartsLoadingContainer />}>
+				<ChartsContainer />
+			</Suspense>
+		</>
+	);
 }
 
-export default page;
+export default AdminPage;
