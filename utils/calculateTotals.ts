08a728be1a5f82b9d9ef254ed0fdd7c287d5d85a
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-21 22:53:01 +0900

    Confirm Route

--- utils/calculateTotals.ts
+++ utils/calculateTotals.ts
@@ -12,8 +12,8 @@ export const calculateTotals = ({
 }: BookingDetails) => {
 	const totalNights = caluculateDaysBetween({ checkIn, checkOut });
 	const subTotal = totalNights * price;
-	const cleaning = 21;
-	const service = 40;
+	const cleaning = 2;
+	const service = 4;
 	const tax = subTotal * 0.1;
 	const orderTotal = subTotal + cleaning + service + tax;
 	return { totalNights, subTotal, cleaning, service, tax, orderTotal };
